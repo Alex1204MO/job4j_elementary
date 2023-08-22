42013b27277b0ce0ff88a9558de1e32432806461
Alex1204MO <4109974@inbox.ru>
2023-08-22 22:13:12 +0300

    Моно строка в матрице. [#214126 #438962] лог

--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class MatrixCheckTest {
-    private MatrixChek MatrixCheck;
 
     @Test
     public void whenHasMonoHorizontal() {
