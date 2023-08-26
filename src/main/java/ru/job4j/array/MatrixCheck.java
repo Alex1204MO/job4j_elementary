b86b9accc1a90f1de0b47f77a3ccc8acc9290b32
Alex1204MO <4109974@inbox.ru>
2023-08-26 21:24:09 +0300

    Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,28 @@ public  class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        int horizontal = 0;
+        int vertical = 0;
+        for (int row = 0; row < board.length; row++) {
+            if (board[row][row] == 'X') {
+                for (int cell = 0; cell < board.length; cell++) {
+                    if (board[row][cell] == 'X') {
+                        horizontal++;
+                    }
+                    if (board[cell][row] == 'X') {
+                        vertical++;
+                    }
+                }
+                if (vertical == board.length || horizontal == board.length) {
+                    result = true;
+                    break;
+                }
+                break;
+            }
+        }
+        return result;
+    }
 }
