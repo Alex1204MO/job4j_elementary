df618ed427dc316ef49d2a51d8ac865e41938230
Alex1204MO <4109974@inbox.ru>
2023-09-01 17:17:04 +0300

    тест

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.converter;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.BDDAssertions.withPrecision;
+
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+    
+
+    @Test
+    void whenConvert200RblThen3dot3333Euro() {
+        float in = 200;
+        float expected = 3.3333f;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+}
