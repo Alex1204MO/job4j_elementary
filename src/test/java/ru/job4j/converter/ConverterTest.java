1821daf7f859d8c00891d929d8300c9c52482a1d
Alex1204MO <4109974@inbox.ru>
2023-09-01 17:38:55 +0300

    тест1

--- src/test/java/ru/job4j/converter/ConverterTest.java
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -1,14 +1,11 @@
 package ru.job4j.converter;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.BDDAssertions.withPrecision;
 
-
 class ConverterTest {
-
     @Test
     void whenConvert140RblThen2Euro() {
         float in = 140;
@@ -17,12 +14,11 @@ class ConverterTest {
         float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
     }
-    
 
     @Test
-    void whenConvert200RblThen3dot3333Euro() {
+    void whenConvert200RblThen2dot3333Euro() {
         float in = 200;
-        float expected = 3.3333f;
+        float expected = 3.333f;
         float out = Converter.rubleToEuro(in);
         float eps = 0.0001f;
         assertThat(out).isEqualTo(expected, withPrecision(eps));
